4e5cf0dc5365f04fad3c170224773bb89bfffc6e
Sri Julapally <sripushkar@gmail.com>
2020-08-07 12:54:16 -0400

    basic setup

--- index.js
+++ index.js
@@ -9,17 +9,9 @@ bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
-bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
 
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
-    }
+bot.on('message', msg => {
+  if(msg.content === ">>>print('hello')"){
+    msg.channel.send("hello")
   }
 });
