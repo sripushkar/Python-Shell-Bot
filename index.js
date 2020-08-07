6ebcaa650f4c73c9534dda4a9f4299a5e511bcfc
Sri Julapally <sripushkar@gmail.com>
2020-08-07 13:57:04 -0400

    error handling

--- index.js
+++ index.js
@@ -13,25 +13,31 @@ bot.on('ready', () => {
 
 bot.on('message', msg => {
   if(isCommand(msg.content)){
-    python = removePrefix(msg.content)
+    python = formatMsg(msg.content)
 
     PythonShell.runString(python, null, function (err, results) {
-      if (err) throw err;
-      msg.channel.send(String(results))
+      if (err){
+        msg.channel.send(String(err));
+      }
+      else{
+        msg.channel.send(String(results))
+      }
       console.log('finished');
     });
-
-
   }
 });
 
 function isCommand(str){
-  if(str.substring(0,3) === ">>>"){
+  if(str.substring(0,1) === ">"){
     return true;
   }
   return false;
 }
 
-function removePrefix(str){
-  return str.substring(3);
+function formatMsg(str){
+  newStr = str.substring(1)
+  newStr = newStr.replace("’", "'");
+  newStr = newStr.replace("“", "\"");
+  newStr = newStr.replace("”", "\"");
+  return newStr;
 }
